00a0a7189bbdca1ecf09b2722f882d2d7761d18c
feblcsack <alifianabdiel@gmail.com>
2025-09-08 23:30:17 +0700

    change theme

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -6,6 +6,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
+import './settings.css';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
